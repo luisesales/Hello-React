6f12e190517ed845f49f3c56ecca89a19450544e
luisesales <duduosales@hotmail.com>
2019-12-19 17:00:14 -0300

    Aula 02 - Versão Completa

--- src/App.js
+++ src/App.js
@@ -2,51 +2,71 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function TarefaLista(props){
-  render(){
-      const title = props.title //=> const {title} = this.props; 
-      const data = props.data; // => const {data} = this.props;  => const {title,data} = this.props;   
-      return(
-        <div>
-          <p>{title}</p>
-          <ul>
-            {data.map((item) => <li>{item}</li>)}
-          </ul>
-        </div>
-      );
-  }
-};
-
-const Tarefa = (props) => {
-  return (
-  <div>
-    <p>{props.title}</p>
-    <input type="text"></input>
-    <button type="submit">Criar Nova Tarefa</button>
-  </div>
+function TarefaLista(props){  
+  const title = props.title //=> const {title} = this.props; 
+  const data = props.data; // => const {data} = this.props;  => const {title,data} = this.props;   
+  return(
+    <div>
+      <p>{title}</p>
+      <ul>
+        {data.map((item) => <li>{item}</li>)}
+      </ul>
+    </div>
   );
 }
 
-onChangeText(){
-  this.setState
+const Tarefa = (props) => {
+  return (<form onSubmit={props.onSubmit}>
+    <p>{props.title}</p>
+    <input
+      type="text" 
+      value={props.text} 
+      onChange={props.onChange}
+    />
+    <button type="submit">
+        Criar nova tarefa
+    </button>
+  </form>);
 }
 
+
+
 class App extends React.Component {
-  
-    state = {Tarefas :[
-      "Aula sobre Componentes",
-      "Aula sobre propriedades e estado",
-      "Outra Aula sobre UI"  
-    ],
-    text : ""
+  state = {Tarefas :[
+    "Aula sobre Componentes",
+    "Aula sobre propriedades e estado",
+    "Outra Aula sobre UI"  
+  ],
+  text : "Nova Tarefa"
+}
+
+  onChangeText(event){
+    this.setState({text : event.target.value});
   }
+  
+  addTask() {
+    const {Tarefas, text} = this.state;
+    Tarefas.push(text);
+    this.setState({
+      Tarefas: Tarefas,
+      text: "Texto da nova tarefa"
+    })
+  }
+    
+    // constructor(props) {
+    //   super(props);
+    //   this.state = [];
+    // }
     render(){
       return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <TarefaLista data={this.state.Tarefas}/>    
-          <Tarefa title={this.state.text} onChange="onChangeText()"/>   
+          <Tarefa title={"Minha Lista de Tarefas"} text={this.state.text} onChange={(event) => this.onChangeText(event)} onSubmit={(event) => {
+            event.preventDefault();
+            this.addTask();
+          }} />   
         </header>
       </div>
     );
