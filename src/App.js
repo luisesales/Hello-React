f5fb720fcb55837226d683e36bfe48900a10c760
luisesales <duduosales@hotmail.com>
2019-12-19 15:57:51 -0300

    Aula 02 Curso de React - Versão incompleta

--- src/App.js
+++ src/App.js
@@ -2,25 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function TarefaLista(props){
+  render(){
+      const title = props.title //=> const {title} = this.props; 
+      const data = props.data; // => const {data} = this.props;  => const {title,data} = this.props;   
+      return(
+        <div>
+          <p>{title}</p>
+          <ul>
+            {data.map((item) => <li>{item}</li>)}
+          </ul>
+        </div>
+      );
+  }
+};
+
+const Tarefa = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <p>{props.title}</p>
+    <input type="text"></input>
+    <button type="submit">Criar Nova Tarefa</button>
+  </div>
   );
+}
+
+onChangeText(){
+  this.setState
+}
+
+class App extends React.Component {
+  
+    state = {Tarefas :[
+      "Aula sobre Componentes",
+      "Aula sobre propriedades e estado",
+      "Outra Aula sobre UI"  
+    ],
+    text : ""
+  }
+    render(){
+      return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <TarefaLista data={this.state.Tarefas}/>    
+          <Tarefa title={this.state.text} onChange="onChangeText()"/>   
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
